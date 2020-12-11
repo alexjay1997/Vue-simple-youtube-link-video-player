53883f89bf9ca43aab318aed648765fd2bfb45db
alexjay1997 <aljayfauni@gmail.com>
2020-12-11 22:27:58 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,62 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Simple Vue Youtube Link Player"/>      
+        <props2 linkss/>
+        
+        <input type="text" v-model.lazy="links" class="inp_link">
+
+        <Head props3 msgVideo/>
+        
+        <p id="demo">{{ links }}</p>
+      
+        <iframe :src="links" v-bind="links" width="500px" height="300px">
+        </iframe>
+
+        <div id="example-2">
+ 
+  <button v-on:click="ChangeBG" class="dark-btn">Dark-Theme</button>
+</div>
+<br><br>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/Header.vue'
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
+    HelloWorld,
+    Head,
+     
+  
+
+ 
+  },
+
+        data()
+  {
+      return{
+      links: 'https://youtube.com/embed/hkpkxq8WGSA'
+    }
+    
+  },
+methods: {
+    ChangeBG: function() {
+      document.body.style.backgroundColor = "#1d1d1d",
+      document.getElementById("app").style.color="#ffffff"
+      alert("Dark Theme enabled")
+
+    }
   }
+    
 }
+
 </script>
+
 
 <style>
 #app {
@@ -24,5 +66,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.inp_link{
+padding:10px;
+border:2px solid #2da2d8;
+border-radius:10px;
+width:400px;
+}
+span{
+  color:grey;
+}
+.dark-btn{
+padding:10px;
+margin-top:15px;
+border-radius:10px;
+border:none;
+color:#ffffff;
+background:purple;
+font-family:calibri;
+font-size:16px;
+cursor:pointer;
+
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h4 props="msgVideo">Ex:https://www.youtube.com/embed/hkpkxq8WGSA</h4>
     
+       <h3 props="linkss"> Enter youtube Link in embed format:</h3><!--this is props-->
+
+  
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
+  name: 'Header',
   props: {
-    msg: String
+    msgVideo: String,
+    linkss : String
+   
+   
   }
 }
+
+
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -29,5 +39,10 @@ li {
 }
 a {
   color: #42b983;
+}
+#video{
+width:500px;
+height:350px;
+border:1px solid red;
 }
 </style>
